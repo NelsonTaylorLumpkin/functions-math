05ba6eabd5db9725189cc0cbbfec45d3a41369fb
Nelson Lumpkin <ossocasa@gmail.com>
2023-03-20 14:15:28 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const addTwo = (num1, num2) => {
+return num1 + num2
+}
+const add = addTwo(2,3)
+console.log(add)
+
+const timesTwo = (num3) => {
+return num3 * 2
+}
+const multiply = timesTwo(add)
+console.log(multiply)
+
+
